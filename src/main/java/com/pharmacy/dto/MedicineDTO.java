c94eb3db909edd53a4aae95ab0242414707b9057
Gunaseelan7704 <guna57275@gmail.com>
2025-07-30 11:23:12 +0530

    change quantity to stock

--- src/main/java/com/pharmacy/dto/MedicineDTO.java
+++ src/main/java/com/pharmacy/dto/MedicineDTO.java
@@ -16,7 +16,7 @@ public class MedicineDTO {
     private String batch;
 
     @PositiveOrZero(message = "Quantity must be zero or positive")
-    private int quantity;
+    private int stock;
 
     @Positive(message = "Price must be positive")
     private double price;
