10faa7097010e30b4b973aaf24fcb7a7f9d086b0
Kishore Kumar <160230351+innocentkishore@users.noreply.github.com>
2025-08-01 20:15:37 +0530

    Update CorsConfig.java

--- src/main/java/com/pharmacy/config/CorsConfig.java
+++ src/main/java/com/pharmacy/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // Allow all endpoints
-                        .allowedOrigins("http://localhost:5173")  // Frontend URL
+                        .allowedOrigins("http://localhost:5174")  // Frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
